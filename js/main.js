807d1c707715a0c6b58311fdf0c9241ffd296248
Matteo Scandolo <matteo.scandolo@link-me.it>
2014-12-15 18:29:10 +0100

    Easy Tech

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var templateStringsButton = document.getElementById('templateStrings');
+
+templateStringsButton.onclick(function(){
+    var name = "Matteo";
+    console.log(`Hello, ${name}!`);
+});
